f83b4a4caf22827d9131f3585cf675669ec67863
momchilmitev <momchilmitev87@gmail.com>
2020-12-17 10:02:10 +0100

    added implementation of dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //   {
@@ -18,11 +19,39 @@ import Search from "./components/Search";
 //   },
 // ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          setSelected={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
